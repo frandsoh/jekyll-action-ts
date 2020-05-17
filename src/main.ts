1f7259291fba1912fa0c54bca4bd34e345f1af09
Frands Otting <frands@otting.dk>
2020-05-17 15:04:41 +0200

    added --trace to jekyll build command

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,7 @@ async function run(): Promise<void> {
         }
         core.debug(`Resolved ${jekyllSrc} as source directory`)
         core.exportVariable('JEKYLL_ENV', 'production')
-        return await exec.exec(`bundle exec jekyll build -s ${jekyllSrc}`)
+        return await exec.exec(`bundle exec jekyll build --trace -s ${jekyllSrc}`)
       }
     })
   } catch (error) {
